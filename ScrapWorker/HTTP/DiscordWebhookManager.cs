f8fe2e44b3202f32267438aa97fc7380113fed43
TheGuy920 <53882381+TheGuy920@users.noreply.github.com>
2024-02-03 19:45:53 -0500

    another

--- ScrapWorker/HTTP/DiscordWebhookManager.cs
+++ ScrapWorker/HTTP/DiscordWebhookManager.cs
@@ -26,13 +26,14 @@ namespace ScrapWorker.HTTP
         private async void StartupMessage(Dictionary<uint, string> Apps)
         {
             this.Apps = Apps;
-            string json = JsonConvert.SerializeObject(Apps, Formatting.Indented).Replace("\"", "\\\"").Replace("\r", "").Replace("\n", "\\n");
-            string content = $"{{\"content\":\"SteamDB tracker started for:\\n```\\n{json}\\n```\", \"flags\": 2}}";
 
             foreach (var obj in WebhookUrlList)
             foreach(var appid in obj.Key)
             if (this.Apps.ContainsKey(appid))
             {
+                string json = JsonConvert.SerializeObject(Apps.Where(i => obj.Key.Contains(i.Key)), Formatting.Indented).Replace("\"", "\\\"").Replace("\r", "").Replace("\n", "\\n");
+                string content = $"{{\"content\":\"SteamDB tracker started for:\\n```\\n{json}\\n```\", \"flags\": 2}}";
+
                 Logger?.WriteLine($"[{this.GetType().FullName}]: Loaded Webhook {obj.Value}");
                 await this.HttpClient.PostAsync(obj.Value, new StringContent(content, MediaTypeHeaderValue.Parse("application/json")));
                 break;
